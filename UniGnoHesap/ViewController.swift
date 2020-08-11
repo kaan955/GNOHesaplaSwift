29a74d0743d83e10484aecdaa4c15bcde0b6a69a
Kaan <kaan.baysal@icloud.com>
2020-08-11 21:18:33 +0300

    Initial Commit

---
+++ UniGnoHesap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniGnoHesap
+//
+//  Created by Kaan Baysal on 11.08.2020.
+//  Copyright © 2020 Kaan Baysal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
